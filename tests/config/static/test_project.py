7d9d70acdf74856b2ba3dc49b7c8d6b36a98b3c5
Bernat Gabor <jokerjokerer@gmail.com>
2017-12-24 16:30:35 +0000

    fixes

--- tests/config/static/test_project.py
+++ tests/config/static/test_project.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import TextIO
 
 import pytest
@@ -6,7 +7,7 @@ from tox3.config import from_toml
 
 
 @pytest.mark.asyncio
-async def load_from_io():
+async def test_load_from_io():
     content = TextIO("""
 [build-system]
 requires = ['setuptools >= 38.2.4']
@@ -16,6 +17,24 @@ build-backend = 'setuptools:build_meta'
   envlist = ['py36']
 """)
     build, project = await from_toml(content)
+    assert build.backend == 'setuptools:build_meta'
+    assert build.requires == ['setuptools >= 38.2.4']
+    assert project == {'envlist': ['py36']}
+
+
+@pytest.mark.asyncio
+async def test_load_from_path(tmpdir):
+    filename: Path = tmpdir / 'test.toml'
+    with open(filename, 'wt') as f:
+        f.write("""
+[build-system]
+requires = ['setuptools >= 38.2.4']
+build-backend = 'setuptools:build_meta'
+
+[tool.tox3]
+  envlist = ['py36']
+""")
+    build, project = await from_toml(filename)
     assert build.backend == 'setuptools:build_meta'
     assert build.requires == ['setuptools >= 38.2.4']
     assert project == {'envlist': ['py36']}
