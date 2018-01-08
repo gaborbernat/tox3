5d13d220e6f5df1988e04f725bda81bc62e1d786
Bernat Gabor <jokerjokerer@gmail.com>
2018-01-08 23:03:54 +0000

    check that python -m tox3 works

---
+++ tests/test_evaluate.py
@@ -0,0 +1,18 @@
+from io import StringIO
+
+import pytest
+
+from tox3.evaluate import main
+from tox3.config.cli import build_parser
+
+
+def test_help(capsys):
+    with pytest.raises(SystemExit) as exit:
+        main(['--help'])
+    assert exit.value.code == 0
+
+    out, err = capsys.readouterr()
+    assert not err
+    help_message_io = StringIO()
+    build_parser().print_help(help_message_io)
+    assert out == help_message_io.getvalue()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,12 @@
+import subprocess
+import sys
 from io import StringIO
 
-import pytest
-
-from tox3.__main__ import main
 from tox3.config.cli import build_parser
 
 
-def test_help(capsys):
-    with pytest.raises(SystemExit) as exit:
-        main(['--help'])
-    assert exit.value.code == 0
-
-    out, err = capsys.readouterr()
-    assert not err
+def test_help():
+    output = subprocess.check_output([sys.executable, '-m', 'tox3', '--help'], universal_newlines=True)
     help_message_io = StringIO()
     build_parser().print_help(help_message_io)
-    assert out == help_message_io.getvalue()
+    assert output == help_message_io.getvalue()
