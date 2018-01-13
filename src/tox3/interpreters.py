99dc83ce0feb2beee7bc6a45e44b48264768837c
Bernat Gabor <gaborjbernat@gmail.com>
2018-01-13 23:43:16 +0000

    add fallback strategies for interpreter discovery

--- src/tox3/interpreters.py
+++ src/tox3/interpreters.py
@@ -1,6 +1,8 @@
 import os
+import re
+import sys
 from pathlib import Path
-from typing import NamedTuple, Tuple
+from typing import Dict, NamedTuple, Optional, Tuple
 
 import py  # type: ignore
 
@@ -21,7 +23,7 @@ class Python(NamedTuple):
 
 
 async def find_python(python: str, logger: Loggers) -> Python:
-    base_python_exe = get_interpreter(python)
+    base_python_exe = get_interpreter(python, logger)
     logger.info('use python %s (as %s)', base_python_exe, python)
     version, version_info = await get_python_info(base_python_exe, logger)
     return Python(python, base_python_exe, version, version_info)
@@ -36,9 +38,51 @@ async def get_python_info(base_python_exe: Path, logger: Loggers) -> Tuple[str, 
     return version, version_info
 
 
-def get_interpreter(name: str) -> Path:
+WIN_32_MAP: Dict[str, Path] = {
+    'python': Path(sys.executable)
+}
+
+
+def get_interpreter(name: str, logger: Loggers) -> Path:
     # noinspection PyCallByClass
-    binary = py.path.local.sysfind(name)
+    binary_str = py.path.local.sysfind(name)
+    if binary_str is not None:
+        return Path(binary_str)
+
+    match = re.match(r"python(\d)\.(\d+)", name)
+    binary = None
+    if match and sys.platform == "win32":
+        major, minor = match.groups()
+        pattern = re.compile(f'python{major}{minor}', re.IGNORECASE)
+        # The standard names are in predictable places.
+        drives = ['C', 'D', 'Z']
+        for drive in drives:
+            base = Path(f'{drive}:\\')
+            for folder in base.iterdir():
+                if pattern.match(folder.name):
+                    binary = folder / 'python.exe'
+                    if binary.exists():
+                        return binary
+                    else:
+                        binary = None
+        if binary is None:
+            binary = locate_via_py(logger, major, minor)
+    else:
+        binary = WIN_32_MAP.get(name, None)
+    if binary is not None and binary.exists():
+        return binary
+
     if binary is None:
         raise RuntimeError('could not find {}, sys path {!r}'.format(name, os.environ.get('PATH')))
     return Path(binary)
+
+
+def locate_via_py(logger: Loggers, v_maj: str, v_min: str) -> Optional[Path]:
+    ver = "-%s.%s" % (v_maj, v_min)
+    py_exe = py.path.local.sysfind('py')
+    if py_exe is not None:
+        printer = CmdLineBufferPrinter(limit=1)
+        result = run([py_exe, ver, '-c', "import sys; print(sys.executable)"], stdout=printer, logger=logger,
+                     exit_on_fail=False)
+        if result:
+            return Path(printer.last)
